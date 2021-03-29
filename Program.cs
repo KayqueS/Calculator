70d26ac505c626309f9a7f41450e6b026d73b4c4
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-29 16:49:49 -0300

    added while structure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Calculator
 {
@@ -8,76 +9,86 @@ namespace Calculator
         {
             int firstNumber = 0;
             int secondNumber = 0;
+            int choice = 0;
 
             Console.WriteLine("---Calculator---");
             Console.WriteLine("----------------");
 
+            Console.WriteLine();
+            Console.WriteLine("[ 1 ] for Sum");
+            Console.WriteLine("[ 2 ] for Subtraction");
+            Console.WriteLine("[ 3 ] for Multiplication");
+            Console.WriteLine("[ 4 ] for Division");
+            Console.WriteLine("[ 5 ] for Rest of Division");
+            Console.WriteLine("[ 6 ] Exit Program");
+
+            Console.WriteLine();
+            Console.WriteLine("What Mathematical Operation do you want to do?");
+            choice = int.Parse(Console.ReadLine());
+
             try
             {
-                Console.WriteLine();
-                Console.WriteLine("[ 1 ] for Sum");
-                Console.WriteLine("[ 2 ] for Subtraction");
-                Console.WriteLine("[ 3 ] for Multiplication");
-                Console.WriteLine("[ 4 ] for Division");
-                Console.WriteLine("[ 5 ] for Rest of Division");
+                while (choice != 6)
+                {
+                    Console.Clear();
 
-                Console.WriteLine();
-                Console.WriteLine("What Mathematical Operation do you want to do?");
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("Enter with two numbers in the same line: ");
+                        string[] vet = Console.ReadLine().Split(' ');
+                        firstNumber = int.Parse(vet[0]);
+                        secondNumber = int.Parse(vet[1]);
+                        Console.WriteLine($"The sum between {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("Enter with two numbers in the same line: ");
+                        string[] vet = Console.ReadLine().Split(' ');
+                        firstNumber = int.Parse(vet[0]);
+                        secondNumber = int.Parse(vet[1]);
+                        Console.WriteLine($"The subtraction between {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
 
-                int choice = int.Parse(Console.ReadLine());
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.WriteLine("Enter with two numbers in the same line: ");
+                        string[] vet = Console.ReadLine().Split(' ');
+                        firstNumber = int.Parse(vet[0]);
+                        secondNumber = int.Parse(vet[1]);
+                        Console.WriteLine($"The multiplication between {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                    }
+                    else if (choice == 4)
+                    {
+                        Console.WriteLine("Enter with two numbers in the same line: ");
+                        string[] vet = Console.ReadLine().Split(' ');
+                        firstNumber = int.Parse(vet[0]);
+                        secondNumber = int.Parse(vet[1]);
+                        Console.WriteLine($"The divison between {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                    }
+                    else if (choice == 5)
+                    {
+                        Console.WriteLine("Enter with two numbers in the same line: ");
+                        string[] vet = Console.ReadLine().Split(' ');
+                        firstNumber = int.Parse(vet[0]);
+                        secondNumber = int.Parse(vet[1]);
+                        Console.WriteLine($"The rest of division between {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: We don't have that option!");
+                    }
 
-                Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("[ 1 ] for Sum");
+                    Console.WriteLine("[ 2 ] for Subtraction");
+                    Console.WriteLine("[ 3 ] for Multiplication");
+                    Console.WriteLine("[ 4 ] for Division");
+                    Console.WriteLine("[ 5 ] for Rest of Division");
+                    Console.WriteLine("[ 6 ] Exit Program");
+                    Console.WriteLine();
 
-                if (choice == 1)
-                {
-                    Console.WriteLine("Enter the first number: ");
-                    firstNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second number: ");
-                    secondNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine(value: $"The sum between {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-                    Console.ReadKey();
-                }
-                else if (choice == 2)
-                {
-                    Console.WriteLine("Enter the first number: ");
-                    firstNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second number: ");
-                    secondNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine(value: $"The subtraction between {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-                    Console.ReadKey();
-
-                }
-                else if (choice == 3)
-                {
-                    Console.WriteLine("Enter the first number: ");
-                    firstNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second number: ");
-                    secondNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine(value: $"The multiplication between {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
-                    Console.ReadKey();
-                }
-                else if (choice == 4)
-                {
-                    Console.WriteLine("Enter the first number: ");
-                    firstNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second number: ");
-                    secondNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine(value: $"The divison between {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-                    Console.ReadKey();
-                }
-                else if (choice == 5)
-                {
-                    Console.WriteLine("Enter the first number: ");
-                    firstNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second number: ");
-                    secondNumber = int.Parse(Console.ReadLine());
-                    Console.WriteLine(value: $"The rest of division between {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Error: We don't have that option!");
-                    Console.ReadKey();
+                    Console.WriteLine("Do you want to do any more Mathematical Operation?");
+                    choice = int.Parse(Console.ReadLine());
                 }
             }
             catch(Exception ex)
